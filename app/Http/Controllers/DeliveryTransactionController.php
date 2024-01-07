223f9a46cef73a62a6b71bfa377cde15b56065f8
yusufarya <aryaherby29nov2k@gmail.com>
2024-01-07 16:04:41 +0700

    fixing bug delivery menu

--- app/Http/Controllers/DeliveryTransactionController.php
+++ app/Http/Controllers/DeliveryTransactionController.php
@@ -18,15 +18,15 @@ class DeliveryTransactionController extends Controller
                             ->select('sales_orders.*', 'order_payments.status as status_payment')
                             ->leftJoin('order_payments', 'sales_orders.code', '=', 'order_payments.order_code')
                             ->orderBy('sales_orders.code', 'DESC')->where(['sales_orders.status'=> 'Y'])->get();
-        // $data2 = ServiceOrder::with('customers', 'sizes')->get();
+        $data2 = ServiceOrder::with('customers')->get();
         // dd($data);
         return view('admin-page.'.$filename, [
             'script' => $filename_script,
             'title' => 'Pengiriman Pesanan ',
             'auth_user' => $user,
             'resultData1' => $data1,
-            // 'resultData2' => $data2
-            'resultData2' => []
+            'resultData2' => $data2
+            // 'resultData2' => []
         ]);
     }
 
